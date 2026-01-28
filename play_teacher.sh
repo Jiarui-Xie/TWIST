620cc76ea17c635880469246c76794d0b8483c21
Yanjie-Ding <dyj175879632@gmail.com>
2026-01-28 23:43:38 +0800

    01-28 push from Yanjie's local TWIST-master

---
+++ play_teacher.sh
@@ -0,0 +1,16 @@
+
+# bash play_teacher.sh 0927_twist_teacher
+
+export CUDA_VISIBLE_DEVICES=0
+
+task_name="g1_priv_mimic"
+proj_name="g1_priv_mimic"
+exptid=$1
+
+cd legged_gym/legged_gym/scripts
+
+# Run the inference script with memory optimization
+python play.py --task "${task_name}" \
+                --proj_name "${proj_name}" \
+                --exptid "${exptid}" \
+                --num_envs 1
